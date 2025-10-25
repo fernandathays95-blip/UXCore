9a97e16f636aa2b3ee086a74ca66e8ccf0834786
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-24 23:09:06 -0300

    Create DesktopUI_iniciar.kt

---
+++ src/main/kotlin/ui/DesktopUI_iniciar.kt
@@ -0,0 +1,26 @@
+package ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DesktopUI_iniciar(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .size(60.dp, 40.dp)
+            .align(Alignment.BottomStart)
+            .background(Color(0x220000FF)) // transparente
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Iniciar", fontSize = 12.sp, color = Color.White)
+    }
+}
