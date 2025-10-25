d61f630ff4172bb289e54cff3145dfe4ec952b8f
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-24 23:19:18 -0300

    Create AppLauncher.java

---
+++ src/main/java/com/arcanos/utils/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.arcanos.utils;
+
+public class AppLauncher {
+    public static void launchApp(String appName) {
+        Logger.log("Launching app: " + appName);
+        // Aqui você pode chamar processos ou simular abertura de apps
+    }
+}
