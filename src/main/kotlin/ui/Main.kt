f6002cf7bc08267534992f981f6d6a084a8ee18f
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-24 23:07:58 -0300

    Update Main.kt

--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -1,10 +1,10 @@
 package ui
 
-import androidx.compose.ui.window.application
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "UXCore Desktop") {
-        DesktopUI() // Chama a interface principal
+        DesktopUI() // componente principal que junta todos os módulos
     }
 }
